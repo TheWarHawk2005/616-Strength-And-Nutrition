5f04db17c21a39f3ded968bb5e63dae12c2b39a8
TheWarHawk2005 <56003863+TheWarHawk2005@users.noreply.github.com>
2024-06-09 15:12:58 -0400

    Create pricing.js

---
+++ pricing.js
@@ -0,0 +1,11 @@
+// Change these values when updating the pricing
+// of the packages.
+
+const pricing = [
+    135,    // strength ONLINE
+    210,    // strength IN PERSON
+    100,    // nutrition ONLINE
+    185,    // strength ONLINE + nutrition ONLINE
+    275,    // strength IN PERSON + nutrition ONLINE
+    75      // camps + consults
+]
