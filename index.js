f7e36b2b98c58eb14801c7844f532439c636bf1e
TheWarHawk2005 <56003863+TheWarHawk2005@users.noreply.github.com>
2024-06-14 17:23:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 elements = document.querySelectorAll('.package-price')
+console.log('Website programmed by Louis Harrison.')
+console.log('Thanks to email.js (emailjs.com), namecheap (namecheap.com) and GitHub (github.com).')
+
 for (i=0;i<elements.length;i++) {
     elements[i].innerHTML = pricing[i].toString()
 }
