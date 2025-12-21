3867f982e7dc45244ddb1d04d5ed9e18a8d2787e
TheWarHawk2005 <56003863+TheWarHawk2005@users.noreply.github.com>
2025-12-20 20:40:55 -0500

    Add files via upload

--- 2025_8.0/index.js
+++ 2025_8.0/index.js
@@ -1,23 +1,52 @@
 console.log('Why hello there. This website programmed by Louis Harrison');
+scrollToHeader()
 
-(function () {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init({
-        publicKey: "Z0XokRkh5OmLpT_4K",
-    });
-})();
+async function scrollToHeader(delay = 1000) {
+    setTimeout(() => {
+        // check for hash in url
+        const scrollToId = window.location.hash.slice(1, window.location.hash.length)
+        if (scrollToId) {
+            document.getElementById(scrollToId).scrollIntoView({ behavior: "smooth" })
+            console.log('Scrolled to', scrollToId);
+        } else {
+            console.log('Provide a #section to scroll a section into view on page load')
+        }
+    },
+        delay)
+}
+
+// copied old code from website
+// TODO: Merge homepage and verify contact box works. MUST BE RAN ON 616strength.com TO PASS CORS
 window.onload = function () {
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
+    const form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', async (event) => {
         event.preventDefault();
-        // these IDs from the previous steps
-        emailjs.sendForm('web_contact_service', 'web_contact_template', this)
-            .then(() => {
-                console.log('SUCCESS!');
-                alert('Message sent! 🏋️‍♂️')
-                location.reload()
-            }, (error) => {
-                console.log('FAILED...', error);
-                alert("Sorry, something went wrong with our email program 🙄. Maybe try again?")
-            });
+
+        const userName = form.querySelector('#user-name').value;
+        const userEmail = form.querySelector('#user-email').value;
+        const contactMessage = form.querySelector('textarea[name="contact-message"]').value;
+
+        console.log({ userName, userEmail, contactMessage });
+
+        (async () => {
+            try {
+                const response = await fetch("https://tests--msmoneypenny.netlify.app/.netlify/functions/moneypenny", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        task: "check_form",
+                        body: { name: userName, email: userEmail, message: contactMessage }
+                    })
+                });
+
+                const data = await response.json();
+                console.log("Function response:", data);
+            } catch (err) {
+                console.error("Fetch failed:", err);
+            }
+        })();
     });
 }
