fbeed6a02edd8c75a3d93e65a272b7b053d737b9
TheWarHawk2005 <56003863+TheWarHawk2005@users.noreply.github.com>
2026-01-01 15:53:31 -0500

    delete batch 2

--- pricing.js
+++
@@ -1,9 +0,0 @@
-// Change these values when updating the pricing
-// of the packages.
-
-const pricing = [
-    150,    // strength ONLINE
-    230,    // strength IN PERSON
-    300,    // strength IN PERSON + nutrition ONLINE
-    85      // camps + consults
-]
